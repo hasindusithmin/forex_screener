abb017e5c510027e1bab4160119ce34ddac5efa9
hasindusithmin <hasindusithmin64@gmail.com>
2022-08-24 16:42:59 +0530

    lightweigth-chart.js has some issues

--- main.py
+++ main.py
@@ -26,44 +26,47 @@ def root(request:Request):
 
 @app.get('/pivot-point/{interval}')
 async def get_pivot_point(base:str,quote:str,interval:str):
-    # To uppercase 
-    base, quote = base.upper(), quote.upper()
-    # start Validate base and quote  
-    cross = investpy.currency_crosses.get_available_currencies()
-    is_valid = True if base in cross else False
-    if not is_valid:
-        raise HTTPException(status_code=400,detail="invaild query(base)")
-    is_valid = True if quote in cross else False
-    if not is_valid:
-        raise HTTPException(status_code=400,detail="invaild query(quote)")
-    # end Validate base and quote 
+    try:
+        # To uppercase 
+        base, quote = base.upper(), quote.upper()
+        # start Validate base and quote  
+        cross = investpy.currency_crosses.get_available_currencies()
+        is_valid = True if base in cross else False
+        if not is_valid:
+            raise HTTPException(status_code=400,detail="invaild query(base)")
+        is_valid = True if quote in cross else False
+        if not is_valid:
+            raise HTTPException(status_code=400,detail="invaild query(quote)")
+        # end Validate base and quote 
 
-    # start Validate interval 
-    intervals = ['5mins', '15mins', '30mins', '1hour', '5hours', 'daily', 'weekly', 'monthly']
-    is_valid = True if interval in intervals else False
-    if not is_valid:
-        raise HTTPException(status_code=400,detail="invaild query(interval)")
-    # end Validate interval 
+        # start Validate interval 
+        intervals = ['5mins', '15mins', '30mins', '1hour', '5hours', 'daily', 'weekly', 'monthly']
+        is_valid = True if interval in intervals else False
+        if not is_valid:
+            raise HTTPException(status_code=400,detail="invaild query(interval)")
+        # end Validate interval 
 
-    # If all condition ok!
-    df = investpy.technical.pivot_points(name=f'{quote}/{base}', country=None, product_type='currency_cross', interval=interval)
-    headers = df.columns.values.tolist()
-    rows = []
-    for index,row in df.iterrows():
-        values = [np.isnan(row[headers[i]]) for i in range(len(row)) if not type(row[headers[i]]) == str]
-        if True in values:
-            continue
-        rows.append({
-            headers[0]:row[headers[0]],
-            headers[1]:row[headers[1]],
-            headers[2]:row[headers[2]],
-            headers[3]:row[headers[3]],
-            headers[4]:row[headers[4]],
-            headers[5]:row[headers[5]],
-            headers[6]:row[headers[6]],
-            headers[7]:row[headers[7]]
-        })
-    return jsonable_encoder(rows)
+        # If all condition ok!
+        df = investpy.technical.pivot_points(name=f'{quote}/{base}', country=None, product_type='currency_cross', interval=interval)
+        headers = df.columns.values.tolist()
+        rows = []
+        for index,row in df.iterrows():
+            values = [np.isnan(row[headers[i]]) for i in range(len(row)) if not type(row[headers[i]]) == str]
+            if True in values:
+                continue
+            rows.append({
+                headers[0]:row[headers[0]],
+                headers[1]:row[headers[1]],
+                headers[2]:row[headers[2]],
+                headers[3]:row[headers[3]],
+                'p':row[headers[4]],
+                headers[5]:row[headers[5]],
+                headers[6]:row[headers[6]],
+                headers[7]:row[headers[7]]
+            })
+        return jsonable_encoder(rows)
+    except:
+        raise HTTPException(status_code=400)
 
 @app.get('/history/{interval}')
 async def get_historical(base:str,quote:str,interval:str):
